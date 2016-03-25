d214b6114db7044f6ed5a163d809523fe35ebe9c
Darrell Sandstrom <dsandstrom@einsteinmedical.com>
2016-03-25 11:38:23 -0700

    Build js

--- vendor/assets/javascripts/jquery.turbolinks.js
+++ vendor/assets/javascripts/jquery.turbolinks.js
@@ -44,6 +44,6 @@ Copyright (c) 2012-2013 Sasha Koss & Rico Sta. Cruz
 
   $.turbo.register();
 
-  $.turbo.use('page:load', 'page:fetch');
+  $.turbo.use('turbolinks:load', 'turbolinks:request-start');
 
 }).call(this);
